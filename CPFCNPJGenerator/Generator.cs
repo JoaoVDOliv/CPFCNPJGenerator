db1f3f9be21defdb92b2e7b03d9e536f578c8655
João Vítor Dorta <olivdortajoao99@gmail.com>
2024-05-28 22:01:55 -0300

    First commit

---
+++ CPFCNPJGenerator/Generator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPFCNPJGenerator
+{
+    public class Generator
+    {
+        bool docType {  get; set; }
+
+        Random random;
+
+        public Generator (bool _docType)
+        {
+            this.docType = _docType;
+            
+            random = new Random ();
+        }
+
+        public string process()
+        {
+            string ret = null;
+                
+            if (this.docType)
+            {
+                return this.generateCPF();
+            }
+
+            return ret;
+        }
+
+        protected string generateCPF()
+        {
+            string cpf = "";
+            
+            for (int i = 1; i <= 11; i++)
+            {
+                cpf = cpf + random.Next(10).ToString();                
+            }
+
+            return cpf;
+        }
+    }
+}
---
+++ CPFCNPJGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using CPFCNPJGenerator;
+
+Generator generator;
+int menuIterator = 0;
+
+Console.WriteLine("Welcome to Document Generator!");
+Console.WriteLine("We generate CPF or CNPJ, as you prefer");
+Console.WriteLine("--------------------------------------");
+do
+{
+    Console.WriteLine("What you want to do?");
+    Console.WriteLine("1. Generate CPF");
+    Console.WriteLine("2. Generate CNPJ");
+    Console.WriteLine("0. Exit");
+    
+    menuIterator = Convert.ToInt32(Console.ReadLine());
+
+    switch (menuIterator)
+    {                
+        case 0:
+            Console.WriteLine("Bye");
+            break;
+
+        case 1:
+            generator = new Generator(true);
+
+            Console.WriteLine(generator.process());
+            break;
+        
+        default:
+            Console.WriteLine("Invalid choice, try again");
+            break;
+    }
+
+} while (menuIterator != 0);
